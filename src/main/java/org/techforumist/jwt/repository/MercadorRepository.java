c7c00fe3375a2b36f5ec7ab404616512375fd399
Alife <contato@tifire.com>
2018-10-24 14:32:50 -0300

    a

--- src/main/java/org/techforumist/jwt/repository/MercadorRepository.java
+++ src/main/java/org/techforumist/jwt/repository/MercadorRepository.java
@@ -14,9 +14,14 @@ public interface MercadorRepository extends JpaRepository<Mercador, Integer>{
 	
 //	@Query(value = "select * from mercador " + "left join itetabpr on itetabpr.codmer = mercador.cod "
 //			+ "where vissit=1 " + "and itetabpr.codtabpre=1" + "order by mercador.des desc ", nativeQuery = true)
-	@Query(value = "select *" + 
+	@Query(value = "select max(cod) cod, codbar, mer" + 
 			" from mercador "
-       + "where mercador.vissit=1 order by mercador.des desc ", nativeQuery = true)
+       + "where mercador.vissit=1 "
+       + "group by codbar, mer " 
+       + "order by mercador.des desc ", 
+       
+       
+       nativeQuery = true)
 	public List<Mercador> listarPorDestaque();
 	
 
