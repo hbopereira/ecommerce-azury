24b9a213faf4a7fd7defb339955b386649c91560
Alife <contato@tifire.com>
2018-10-26 12:04:55 -0300

    a

--- src/main/resources/static/app/controllers/begin.js
+++ src/main/resources/static/app/controllers/begin.js
@@ -17,11 +17,11 @@ angular.module('JWTDemoApp')
 		} ]
 	};
 
-	var selecionar = function(key) {
-		if (!$scope.selecionados[key])
-			$scope.selecionados[key] = 0;
-		$scope.selecionados[key]++;
-	}
+	//$scope.selecionar = function(key) {
+		//if (!$scope.selecionados[key])
+			//$scope.selecionados[key] = 0;
+		//$scope.selecionados[key]++;
+	//}
 	
 	
 
@@ -66,6 +66,5 @@ angular.module('JWTDemoApp')
 	// listarProdutosPorTabelaPrecoSecaoVestido();
 	// listarProdutosPorTabelaPrecoSecaoCigana();
 	// listarProdutosPorTabelaPrecoSecaoCamisete();
-	selecionar();
 
 });
