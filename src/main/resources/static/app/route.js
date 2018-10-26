77431dbe1463635b9cd4092919f01593a3f7b272
Alife <contato@tifire.com>
2018-10-26 17:59:49 -0300

    a

--- src/main/resources/static/app/route.js
+++ src/main/resources/static/app/route.js
@@ -66,7 +66,7 @@ angular.module('JWTDemoApp').config(function($stateProvider, $urlRouterProvider)
 		views : {
 			'content@' : {
 				templateUrl : 'app/views/secmer/secmerVestido.html',
-				controller : 'BeginController'
+				controller : 'SecmerController'
 			}
 		}
 	}).state('secmer-cigana', {
@@ -75,7 +75,7 @@ angular.module('JWTDemoApp').config(function($stateProvider, $urlRouterProvider)
 		views : {
 			'content@' : {
 				templateUrl : 'app/views/secmer/secmerCigana.html',
-				controller : 'BeginController'
+				controller : 'SecmerController'
 			}
 		}
 	}).state('secmer-camisete', {
